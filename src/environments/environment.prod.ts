2d79325ede812e7cc0a88ac02b7424aa0aae5a38
senthil.a <senthil.a@kgisl.com>
2023-03-27 19:28:34 +0530

    cordova integerated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCRQZa0FzVGw7iWSEOMFFsj-7ODQi3u6ic",
+    authDomain: "bankingapp-2b8d1.firebaseapp.com",
+    projectId: "bankingapp-2b8d1",
+    storageBucket: "bankingapp-2b8d1.appspot.com",
+    messagingSenderId: "687129174377",
+    appId: "1:687129174377:web:bc6e24c14359ec8e9b5df8",
+    measurementId: "G-WBECFP6VF8"
+  }
 };
